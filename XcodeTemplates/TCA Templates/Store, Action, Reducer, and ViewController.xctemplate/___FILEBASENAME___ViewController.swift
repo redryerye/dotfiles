338a45b7ced8fd83ffec703d1e5270b3df4128fa
redryerye <yymmt3.waste@gmail.com>
2021-10-27 17:55:22 +0900

    Add Xcode templates

---
+++ XcodeTemplates/TCA Templates/Store, Action, Reducer, and ViewController.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,20 @@
+import ComposableArchitecture
+import UIKit
+
+class ___VARIABLE_productName:identifier___ViewController: UIViewController {
+  private let store: Store<___VARIABLE_productName:identifier___State, ___VARIABLE_productName:identifier___Action>
+  
+  init(store: Store<___VARIABLE_productName:identifier___State, ___VARIABLE_productName:identifier___Action>) {
+    self.store = store
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
